ed9ed6d87007a2d3f2d027041c4da8ef6a9130dd
Dip Halder <47663440+Diphalder@users.noreply.github.com>
2022-12-31 03:54:32 +0600

    bi

--- AI lab final/bidirectional.py
+++ AI lab final/bidirectional.py
@@ -4,12 +4,12 @@ def bidirectional(graph,revgraph, start, end):
   global visit1
   global visit2
 
-  queue_start = [(start, [start])]
-  queue_end = [(end, [end])]
+  queue_forward = [(start, [start])]
+  queue_backward = [(end, [end])]
   
-  while (queue_start and queue_end):
-    (u1, path_forward) = queue_start.pop(0)
-    
+  while (queue_forward and queue_backward):
+    (u1, path_forward) = queue_forward.pop(0)
+    (u2, path_backward) = queue_backward.pop(0)
 
     if visit2[u1]:  
       return path_forward + list(reversed(path_backward))
@@ -17,23 +17,17 @@ def bidirectional(graph,revgraph, start, end):
     visit1[u1]=1
     for v in graph[u1]:
       if(visit1[v]==0):
-        queue_start.append((v, path_forward + [v]))
+        queue_forward.append((v, path_forward + [v]))
     
 
 
-
-
-
-
-    (u2, path_backward) = queue_end.pop(0)
-    
     if visit1[u2]:  
       return path_forward + list(reversed(path_backward))
     
     visit2[u2]=1
     for v in revgraph[u2]:
       if(visit2[v]==0):
-        queue_end.append((v, path_backward + [v]))
+        queue_backward.append((v, path_backward + [v]))
   return None
     
 vertex=[]
