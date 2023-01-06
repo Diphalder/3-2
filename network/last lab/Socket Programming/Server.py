8ceefca7a807f2982f477f9320359a4fa75ac6e9
Dip Halder <47663440+Diphalder@users.noreply.github.com>
2023-01-06 22:50:52 +0600

    k

---
+++ network/last lab/Socket Programming/Server.py
@@ -0,0 +1,40 @@
+import socket 
+
+HOST='127.0.0.1'
+# HOST='127.0.0.1' when no internet connection LOCAL host
+PORT= 9999
+
+server= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# af -> address family internet type
+#2 type of stream 
+# #for UDP socket_stream
+
+server.bind((HOST,PORT))
+
+# ready for communication
+
+server.listen(5)
+
+# ON for client .. client can send rqst
+#queue lenth is 5
+
+
+
+while True:  # give client unlimited time
+    communication_socket,address = server.accept()   #    , client site ip address
+    print(f"Connected to {address}") 
+    while 1:
+        message = communication_socket.recv(1024).decode('utf-8')      # at e time 1024byte can recive
+        if(message=="exit"):
+            break
+        print(f"Message from Client is: {message}") 
+    communication_socket.send(f"Got your message! Thank you".encode('utf-8'))
+    communication_socket.close()
+    print(f"Connection with {address} ended")
+
+
+
+
+
+
+
---
+++ network/last lab/Socket Programming/client.py
@@ -0,0 +1,21 @@
+import socket 
+
+HOST='127.0.0.1' # server address
+
+PORT=9999 # which port of the server what to communicate
+
+socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# send rqst and get ack 
+
+
+socket.connect((HOST,PORT))
+
+while 1:
+    message=input("Enter your Message = ")
+    socket.send(message.encode('utf-8'))   # formating
+    if(message=="exit"):
+        break
+print(socket.recv(1024).decode('utf-8')) 
+
+
+
